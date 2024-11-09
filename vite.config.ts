c067338cd0ccc9cf32445e2404fe839268f286b5
Klejdis2003 <klejdis36@gmail.com>
2024-11-09 18:26:28 -0500

    Changed basename

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import TanStackRouterVite from '@tanstack/router-plugin/vite';
 import * as path from 'node:path';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite()],
+  base: '/miscellaneous',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
