05602f4449ab7d18355859cd680cec95e88fc85b
Klejdis2003 <klejdis36@gmail.com>
2024-11-09 18:04:50 -0500

    Attempting deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import TanStackRouterVite from '@tanstack/router-plugin/vite';
 import * as path from 'node:path';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), ghPages(), TanStackRouterVite()],
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
